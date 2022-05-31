e731f54dd022436c5d06103772cc5c8d7489cc6c
nucpopp <kjm5546@gmail.com>
2022-05-31 22:47:25 +0900

    update project

---
+++ android/app/src/main/java/com/nucpoop/flutter_study/MainActivity.java
@@ -0,0 +1,6 @@
+package com.nucpoop.flutter_study;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
--- android/app/src/main/kotlin/com/example/bo_study/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.bo_study
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
